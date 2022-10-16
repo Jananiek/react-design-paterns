0ac110f027277fcd2dd40130e54cf8247578023f
Jananie Wathsala <jananie.kum@gmail.com>
2022-10-16 19:32:04 +0800

    split screen component (#4)
    * Beginning code for video
    
    * Moving files  using main
    
    * add split screen component

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { SplitScreen } from './SplitScreen';
+const LeftHandComponent=({name})=>{
+  return (
+    <h1 style={{background:'green'}}>{name}!</h1>
+  )
+}
+const RightHandComponent=({message})=>{
+  return (<p style={{background:'red'}}>{message}</p>)
+}
+
+function App() {
+  return (
+   <SplitScreen leftWeight={1} rightWeight={3}>
+    <LeftHandComponent name='Jananie'/>
+    <RightHandComponent message='Greetings from admin'/>
+   </SplitScreen>
+  );
+}
+
+export default App;
