35fa18aedcc79f9537c6e1c33ef466f001e97493
jananie-wathsala <jananie.kum@gmail.com>
2022-10-16 18:43:41 +0530

    resource loader to get data from any resource

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { UserLoader } from "./UserLoader";
+import { UserInfo } from "./UserInfo";
+import { ProductInfo } from "./ProductInfo";
+import {ResourceLoader} from './ResourceLoader'
 function App() {
-	return (
-		<h1>Your code goes here</h1>
-	);
+  return (
+    <>
+	<h3>==================specific Loader=====================</h3>
+      <UserLoader userId="234">
+        <UserInfo />
+      </UserLoader>
+
+      <UserLoader userId="345">
+        <UserInfo />
+      </UserLoader>
+	  <h3>==================any Loader from Resource Loader=====================</h3>
+	  <ResourceLoader resourceUrl="/users/345" resourceName="user">
+	  <UserInfo />
+	  </ResourceLoader>
+
+	  <ResourceLoader resourceUrl="/products/1234" resourceName="product">
+	  <ProductInfo />
+	  </ResourceLoader>
+    </>
+  );
 }
 
 export default App;
