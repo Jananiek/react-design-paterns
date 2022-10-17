6aa09b311dea7a99971d1d2c7026ef2f5fba99d1
jananie-wathsala <jananie.kum@gmail.com>
2022-10-17 18:32:48 +0530

    uncontrolled on boarding flow

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import { useState } from 'react';
-import { UncontrolledForm } from './UncontrolledForm';
-import { ControlledForm } from './ControlledForm';
-import { UncontrolledModal } from './UncontrolledModal';
-import { ControlledModal } from './ControlledModal';
+import { useState } from "react";
+import { UncontrolledOnBoardingFlow } from "./UncontrolledOnboardingFlow";
 
+const StepOne = ({ goToNext }) => {
+  return (
+    <>
+      <h1>Step 1</h1>
+      <button onClick={() => goToNext({ name: "John" })}>Next</button>
+    </>
+  );
+};
+const StepTwo = ({ goToNext }) => {
+  return (
+    <>
+      <h1>Step 2</h1>
+      <button onClick={() => goToNext({ age: 30 })}>Next</button>
+    </>
+  );
+};
+const StepThree = ({ goToNext }) => {
+  return (
+    <>
+      <h1>Step 3</h1>
+      <button onClick={() => goToNext({ hairColor: "brown" })}>Next</button>
+    </>
+  );
+};
 function App() {
-	const [shouldShowModal, setShouldShowModal] = useState(false);
-
-	return (
-		<>
-		<ControlledModal
-			shouldShow={shouldShowModal}
-			onRequestClose={() => setShouldShowModal(false)}
-		>
-			<h1>Hello!</h1>
-		</ControlledModal>
-		<button onClick={() => setShouldShowModal(!shouldShowModal)}>
-			{shouldShowModal ? 'Hide Modal' : 'Show Modal'}
-		</button>
-		</>
-	);
+  return (
+    <UncontrolledOnBoardingFlow
+      onFinish={data => {
+        console.log(data);
+        alert("finish onboarding");
+      }}
+    >
+      <StepOne />
+      <StepTwo />
+      <StepThree />
+    </UncontrolledOnBoardingFlow>
+  );
 }
 
 export default App;
