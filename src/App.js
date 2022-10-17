4fc43e8c3bdadcf53c154e0db67cbf2b1ee119d1
jananie-wathsala <jananie.kum@gmail.com>
2022-10-17 17:11:43 +0530

    controlled & uncontrolled forms

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import {UncontrolledForm} from "./UncontrolledForm"
+import {ControlledForm} from "./ControlledForm"
 function App() {
 	return (
-		<h1>Your code goes here</h1>
+    <>
+    <UncontrolledForm/>
+		<ControlledForm/></>
+		
 	);
 }
 
