bb8d03209662b069a28dcef043a1bce657f6ce0e
jananie-wathsala <jananie.kum@gmail.com>
2022-10-18 22:17:29 +0530

    HO component with data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { printProps } from "./PrintProps";
+import { UserInfo } from "./UserInfo";
+import { withUser } from "./withUser";
+import { UserInfoForm } from "./UserInfoForm";
+
+const UserInfoWrapped = printProps(UserInfo);
+const UserInfoWithLoader=withUser(UserInfo, "234")
 function App() {
-	return (
-		<h1>Your code goes here</h1>
-	);
+  return (<>
+  <h2>UserInfoWrapped</h2>
+   <UserInfoWrapped a={1} b="Hello" c={{ name: "John" }} />
+
+   <h2>UserInfoWithLoader</h2>
+    <UserInfoWithLoader />
+
+    <h2>UserWithEditableData</h2>
+    <UserInfoForm/>
+    </>
+   
+  );
 }
 
 export default App;
